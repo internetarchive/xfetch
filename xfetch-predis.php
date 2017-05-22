924a3c5296df84a09642b7f898387e342f32c1f1
Jim Nelson <jnelson@archive.org>
2017-05-22 13:32:30 -0700

    Add XFetch support
    Also, don't gather stats on first-round of workers.
    
    Refactoring in the works.

--- xfetch-predis.php
+++ xfetch-predis.php
@@ -9,8 +9,8 @@ Predis\Autoloader::register();
 /**
  * Times in seconds.
  */
-define('TTL', 10);
-define('DELTA', 5);
+define('TTL', 20);
+define('DELTA', 2);
 define('LOCK_TTL', 10);
 
 /**
@@ -22,12 +22,18 @@ define('EARLY', 2);
 define('UNKNOWN', 127);
 
 /**
- * Test parameters.
+ * Redis keys.
  */
 define('REDIS_KEY', 'test:cache');
 define('LOCK_KEY', 'test:lock');
+define('DELTA_KEY', 'test:delta');
+
+/**
+ * Test parameters.
+ */
 define('WORKERS', 50);
 define('REPORT_EVERY_SEC', 5);
+define('BETA', 1.0);
 
 // run it!
 exit(main($argv));
@@ -45,10 +51,10 @@ function main($argv)
 
   // clear the cached value from Redis
   $redis = new Predis\Client();
-  $redis->del(REDIS_KEY);
-  $redis->del(LOCK_KEY);
+  $redis->del(REDIS_KEY, LOCK_KEY, DELTA_KEY);
 
   $workers = [];
+  $first_pass = true;
 
   $total = 0;
   $tally = [ 'hits' => 0, 'misses' => 0, 'earlies' => 0, 'error' => 0 ];
@@ -59,43 +65,54 @@ function main($argv)
     // keep max. workers running
     $added = 0;
     while (!$halt && count($workers) < WORKERS) {
-      $worker = new LockWorker();
+      $worker = new XFetchWorker();
+      $worker->first_pass = $first_pass;
+
       $pid = $worker->start();
 
       $workers[$pid] = $worker;
       $added++;
     }
 
-    // if added in bulk, give child processes a chance to start
-    if ($added > 1)
-      sleep(1);
+    // reset to indicate new workers are post-first-pass (which plays into stats gathering)
+    $first_pass = false;
+
+    // if child processes were added in bulk, give them chance to start
+    //if ($added > 1)
+      usleep(1 * 1000);
 
     // wait for a child process to exit
     $pid = pcntl_wait($status);
     if ($pid === -1)
       exit("Could not wait for child process\n");
 
+    // remove from pool and get exit code
+    $worker = $workers[$pid];
     unset($workers[$pid]);
     $exitcode = pcntl_wifexited($status) ? pcntl_wexitstatus($status) : UNKNOWN;
 
-    $total++;
-    switch ($exitcode) {
-      case HIT:
-        $tally['hits']++;
-      break;
+    // don't gather stats for the first pass; this allows for stats to be gathered only once the
+    // cache is warm
+    if (!$worker->first_pass) {
+      $total++;
+      switch ($exitcode) {
+        case HIT:
+          $tally['hits']++;
+        break;
 
-      case MISS:
-        $tally['misses']++;
-      break;
+        case MISS:
+          $tally['misses']++;
+        break;
 
-      case EARLY:
-        $tally['earlies']++;
-      break;
+        case EARLY:
+          $tally['earlies']++;
+        break;
 
-      case UNKNOWN:
-      default:
-        $tally['error']++;
-      break;
+        case UNKNOWN:
+        default:
+          $tally['error']++;
+        break;
+      }
     }
 
     // report stats every 'n' seconds, but only report when a full round of workers have completed
@@ -104,18 +121,25 @@ function main($argv)
       var_dump($tally);
       echo "\n";
 
+      // calc next report time
       $report_time_t = time() + REPORT_EVERY_SEC;
     }
 
-    pcntl_signal_dispatch();
+    // allow for signals to be dispatched ... this is used in place of declare(ticks=1)
+    if (!$halt)
+      pcntl_signal_dispatch();
   } while (!$halt || count($workers));
 
+  // final report
   echo "$total samples:\n";
   var_dump($tally);
 
   return 0;
 }
 
+/**
+ * Dummy recompute function.
+ */
 function recompute_fn()
 {
   sleep(DELTA);
@@ -212,5 +236,53 @@ class LockWorker extends ChildWorker
   private function release_lock($redis)
   {
     $redis->del(LOCK_KEY);
+  }
+}
+
+/**
+ *
+ */
+
+class XFetchWorker extends ChildWorker
+{
+  protected function run($redis)
+  {
+    $result = HIT;
+
+    $pipe = $redis->pipeline(['atomic' => true]);
+    $pipe->ttl(REDIS_KEY);
+    $pipe->get(REDIS_KEY);
+    $pipe->get(DELTA_KEY);
+    list($ttl, $value, $delta) = $pipe->execute();
+
+  if (!$value || self::xfetch($delta, $ttl)) {
+      $result = !$value ? MISS : EARLY;
+
+      $start = microtime(true);
+      $value = recompute_fn();
+      $delta = microtime(true) - $start;
+
+      $pipe = $redis->pipeline(['atomic' => true]);
+      $pipe->setex(REDIS_KEY, TTL, $value);
+      $pipe->setex(DELTA_KEY, TTL, $delta);
+      $pipe->execute();
+    }
+
+    return $result;
+  }
+
+  private static function xfetch($delta, $ttl)
+  {
+    $now = time();
+    $expiry = $now + $ttl;
+    $rnd = log(mt_rand() / mt_getrandmax());
+
+    $xfetch = $delta * BETA * $rnd;
+
+    $recompute = (time() - $xfetch) >= $expiry;
+    if ($recompute)
+      echo "* early recompute! delta:$delta ttl:$ttl rnd:$rnd xfetch:$xfetch\n";
+
+    return $recompute;
   }
 }
